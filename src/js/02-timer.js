e1916262dba5815b23557749d75498626ca01c41
vl-brrr <vlada.brayko@gmail.com>
2023-11-25 22:49:11 +0200

    Triger

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -46,7 +46,6 @@ values.forEach(value => {
   value.style.fontSize = '28px';
 });
 
-//
 const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
