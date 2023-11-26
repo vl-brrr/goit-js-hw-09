2585ee82213ade171b09ddc7a87baa8c2d3eb8b3
vl-brrr <vlada.brayko@gmail.com>
2023-11-26 15:02:15 +0200

    All finally working

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const form = document.querySelector('.form');
 form.addEventListener('submit', hadleSubmit);
@@ -8,19 +9,23 @@ function hadleSubmit(event) {
   const { delay, step, amount } = event.target.elements;
   let j = +delay.value;
 
-  for (let i = 1; i <= +amount.value; i++) {
-    createPromise(i, j)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-          timeout: 5000,
+  if (+delay.value < 0 || +step.value < 0 || +amount.value <= 0) {
+    Report.failure('Error', 'Please enter a value of 0 or greater');
+  } else {
+    for (let i = 1; i <= +amount.value; i++) {
+      createPromise(i, j)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+            timeout: 5000,
+          });
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+            timeout: 5000,
+          });
         });
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-          timeout: 5000,
-        });
-      });
-    j += +step.value;
+      j += +step.value;
+    }
   }
 }
 
